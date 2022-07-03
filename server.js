7a2d28c6d5b26e02f54936e7d5b7cd507d49279c
Hamza Raza <hamza@hamzaraza.com>
2022-07-03 15:55:26 +0900

    fix

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const knex = require('knex')
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      connectionString: process.env.DATABASE_URL,
       ssl: {
         rejectUnauthorized: false
       }
